c020c0b351d508176fd631d6efb4ab99dae4bbc1
Jonas <jonasd1305@gmail.com>
2022-01-10 20:42:48 +0100

    Initial commit

---
+++ src/main/java/at/demski/blockfabrik_stats_page/BlockfabrikStatsPageApplication.java
@@ -0,0 +1,13 @@
+package at.demski.blockfabrik_stats_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockfabrikStatsPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockfabrikStatsPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/demski/blockfabrik_stats_page/BlockfabrikStatsPageApplicationTests.java
@@ -0,0 +1,13 @@
+package at.demski.blockfabrik_stats_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockfabrikStatsPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
