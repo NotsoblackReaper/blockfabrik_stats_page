4c1c1194bc9e072c7bebb9c4a2316fc52e1d726e
Jonas <jonasd1305@gmail.com>
2022-01-18 14:25:55 +0100

    Added args to enable data scraping and webserver

--- src/main/java/at/demski/blockfabrik_stats_page/BlockfabrikStatsPageApplication.java
+++ src/main/java/at/demski/blockfabrik_stats_page/BlockfabrikStatsPageApplication.java
@@ -1,15 +1,27 @@
 package at.demski.blockfabrik_stats_page;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 @EnableScheduling
 public class BlockfabrikStatsPageApplication {
 
+    public static boolean data_scraping;
+
     public static void main(String[] args) {
-        SpringApplication.run(BlockfabrikStatsPageApplication.class, args);
+        data_scraping = Boolean.parseBoolean(args[0]);
+        boolean web_server = Boolean.parseBoolean(args[1]);
+        System.out.println("Configuration:\nData Scraping:\t"+data_scraping+"\nWeb Server:\t"+web_server);
+        if (web_server)
+            SpringApplication.run(BlockfabrikStatsPageApplication.class, args);
+        else
+            new SpringApplicationBuilder(BlockfabrikStatsPageApplication.class)
+                    .web(WebApplicationType.NONE)
+                    .run(args);
     }
 
 }
